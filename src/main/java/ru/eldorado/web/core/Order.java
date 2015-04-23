aeaf9c9bb18d14ab1aa1d44ccfe96be114f5bc8d
Mikhail Maxaymer <Mikhail.Maxaymer@eldorado.ru>
2015-04-23 15:32:40 +0300

    Remove bad query

--- src/main/java/ru/eldorado/web/core/Order.java
+++ src/main/java/ru/eldorado/web/core/Order.java
@@ -39,44 +39,6 @@ public class Order extends CoreObject {
                     " p_email as "+ USEREMAIL+", p_name as "+ USERNAME+","+
                     " p_phonenumber as "+ USERPHONE+" from {order} where {order.code}=?";
 
-//    private static final String QUERY_ORDER_BY_ID =
-//            "select createdts as " + CREATEDATE + "," + TOTALPRICE + ", " +
-//                    "({" +
-//                    "   {" +
-//                    "       select count(distinct {product}) " +
-//                    "       from {orderentry join order on {orderentry.order} = {order.pk} and {order.code} = ?}" +
-//                    "   }" +
-//                    "}) as " + PRODUCTNUMBER + "," + SUBTOTAL + "," + DELIVERYCOST + ", " +
-//                    "({" +
-//                    "   {" +
-//                    "       select code from {deliverymode} where {deliverymode.pk} in (" +
-//                    "           {" +
-//                    "               {" +
-//                    "                   select deliverymodepk from {order} where {order.code} = ?" +
-//                    "               }" +
-//                    "           }" +
-//                    "       )" +
-//                    "   }" +
-//                    "}) as " + DELIVERYMODE + ", " +
-//                    "({" +
-//                    "   {" +
-//                    "       select {enumerationvalue.code} as " + STATUS + " " +
-//                    "       from {enumerationvalue join order on {enumerationvalue.pk} = {order.status} and {order.code} = ?}" +
-//                    "   }" +
-//                    "}) as " + STATUS + "," +
-//                    "({" +
-//                    "   {" +
-//                    "       select {address.streetname} from {address} where {pk} = " +
-//                    "           ({" +
-//                    "               {" +
-//                    "                   select {pointofservice.address} " +
-//                    "                   from {pointofservice join order on {pointofservice.pk} = {order.pointofservice} and {order.code} = ?}" +
-//                    "               }" +
-//                    "           })" +
-//                    "   }" +
-//                    "}) as " + POINTOFSERVICE + ", p_email as " + USEREMAIL + ", p_name as " + USERNAME + "," +
-//                    " p_phonenumber as " + USERPHONE + " from {order} where {order.code} = ?";
-
     public static Order getInstance() {
         Order order = INSTANCE.get();
         if (order == null) {
